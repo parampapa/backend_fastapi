01f57a484b3b3bcdf391bb35d4aa910d2b31064d
admin <alexandrpeks@gmail.com>
2025-10-02 22:58:16 +0400

    Схемы вынесены в отдельную директорию, реализована пагинация в get.

---
+++ hotels.py
@@ -0,0 +1,86 @@
+from fastapi import Query, Body, APIRouter
+from schemas.hotels import Hotel, HotelPATCH
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
+]
+
+
+@router.get("")
+def get_hotels(
+    id: int | None = Query(None),
+    title: str | None = Query(None),
+    page: int = Query(1, ge=1),
+    per_page: int = Query(3, ge=1, le=100),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+
+    # пагинация всегда работает, даже если параметры не переданы
+    start = (page - 1) * per_page
+    end = start + per_page
+    items = hotels_[start:end]
+
+    return {
+        "page": page,
+        "per_page": per_page,
+        "total": len(hotels_),
+        "total_pages": (len(hotels_) + per_page - 1) // per_page,
+        "items": items,
+    }
+
+
+@router.post("")
+def create_hotel(hotel_data: Hotel):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": hotel_data.title
+    })
+    return {"status": "OK"}
+
+
+@router.put("/{hotel_id}")
+def edit_hotel(hotel_id: int, hotel_data: Hotel):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    hotel["title"] = hotel_data.title
+    hotel["name"] = hotel_data.name
+    return {"status": "OK"}
+
+
+@router.patch(
+    "/{hotel_id}",
+    summary="Частичное обновление данных об отеле",
+    description="<h1>Тут мы частично обновляем данные об отеле: можно отправить name, а можно title</h1>",
+)
+def partially_edit_hotel(
+        hotel_id: int, hotel_data: HotelPATCH,
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if hotel_data.title:
+        hotel["title"] = hotel_data.title
+    if hotel_data.name:
+        hotel["name"] = hotel_data.name
+    return {"status": "OK"}
+
+
+@router.delete("/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
