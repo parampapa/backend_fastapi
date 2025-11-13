6d1e00489d580298288e7fd0adcec228e039267c
admin <alexandrpeks@gmail.com>
2025-11-13 23:57:28 +0400

    added endpoint auth.py, repo - users and schema - users

--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
+from src.api.auth import router as router_auth
 
 app = FastAPI()
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
