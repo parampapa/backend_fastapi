147ba93b156a05323ca953e3eb969e08ee00fbb4
admin <alexandrpeks@gmail.com>
2025-11-15 00:19:26 +0400

    Added a check for the presence of an email in the database when authenticating a user through the repository

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,8 +1,19 @@
+from sqlalchemy import select
+from pydantic import EmailStr
+
 from src.models.users import UsersOrm
 from src.repositories.base import BaseRepository
-from src.schemas.users import User
+from src.schemas.users import User, UserWithHashedPassword
 
 
 class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
+
+    async def get_user_with_hashed_password(self, email: EmailStr) -> UserWithHashedPassword:
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        return UserWithHashedPassword.model_validate(model)
