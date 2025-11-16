5a1c4bd386c4475320c833d3ef8de79af5b8ecc5
admin <alexandrpeks@gmail.com>
2025-11-16 22:44:21 +0400

    Added a router to receive a token.

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,7 +1,4 @@
-import os
-from datetime import datetime, timezone, timedelta
-
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
@@ -40,3 +37,12 @@ async def login_user(
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token, httponly=True)
     return {"access_token": access_token}
+
+@router.get("/only_auth")
+async def get_only_auth(
+        request: Request
+):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        print("нету тут токена")
+    print(access_token)
