e3b84ef184000d6628e4af29e26061d84372abf9
admin <alexandrpeks@gmail.com>
2025-11-26 23:19:21 +0400

    Added get_filtered to get get_all with filters

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,10 +1,6 @@
 from sqlalchemy import select, insert, update, delete
 from pydantic import BaseModel
 
-from src.database import async_session_maker
-from src.models.hotels import HotelsOrm
-from src.schemas.hotels import Hotel
-
 
 class BaseRepository:
     model = None
@@ -13,11 +9,14 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
-    async def get_all(self, *args, **kwargs):
-        query = select(self.model)
+    async def get_filtered(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
+        return [self.schema.model_validate(model, from_attributes=True) for model in
+                result.scalars().all()]
 
-        return [self.schema.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+    async def get_all(self, *args, **kwargs):
+        return await self.get_filtered()
 
     async def get_one_or_none(self, **filter_by):
         query = select(self.model).filter_by(**filter_by)
