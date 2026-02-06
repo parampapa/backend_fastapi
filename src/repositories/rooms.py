7728dba31139e74cf49b0fca5d5da2d980ba08e9
admin <alexandrpeks@gmail.com>
2026-02-06 21:22:14 +0400

    add new method for get rooms on time

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,10 +1,8 @@
-from typing import List
-
-from pydantic import BaseModel
-from sqlalchemy import select
+from datetime import date
 
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.rooms import Room
 
 
@@ -12,3 +10,12 @@ class RoomsRepository(BaseRepository):
     model = RoomsOrm
     schema = Room
 
+    async def get_filtered_by_time(
+            self,
+            hotel_id: int,
+            date_from: date,
+            date_to: date,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(hotel_id, date_from, date_to)
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
